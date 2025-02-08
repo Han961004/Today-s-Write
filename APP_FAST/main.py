5f374db4ec5f882789183b4a55661d1da7baec7c
Dong-Han <donghk77@naver.com>
2025-02-08 23:24:38 +0900

    feat : add prometheus for fast

--- APP_FAST/main.py
+++ APP_FAST/main.py
@@ -1,10 +1,13 @@
 from fastapi import FastAPI
 from routers import posts
+from prometheus_fastapi_instrumentator import Instrumentator
 
 app = FastAPI(title="FastAPI Application")
 
 # 라우터 등록
 app.include_router(posts.router)
+instrumentator = Instrumentator()
+instrumentator.instrument(app).expose(app, endpoint="/api/metrics")
 
 @app.get("/")
 def read_root():
