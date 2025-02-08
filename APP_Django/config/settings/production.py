b0ebd9b7e52ecc70ee4818adad5bfed14e821417
Dong-Han <donghk77@naver.com>
2025-02-08 22:34:50 +0900

    fix

--- APP_Django/config/settings/production.py
+++ APP_Django/config/settings/production.py
@@ -7,11 +7,11 @@ ALLOWED_HOSTS = ['43.200.252.76', "web", "localhost", "127.0.0.1"]
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'app_django',
-        'USER': 'postgres',
-        'PASSWORD': '0000',
-        'HOST': 'db',  # docker-compose 서비스 이름
-        'PORT': '5432',
+        'NAME': os.environ.get('POSTGRES_DB', 'app_django'),
+        'USER': os.environ.get('POSTGRES_USER', 'postgres'),
+        'PASSWORD': os.environ.get('POSTGRES_PASSWORD', '0000'),
+        'HOST': os.environ.get('DATABASE_HOST', 'django_db'),  # 여기 수정!
+        'PORT': os.environ.get('POSTGRES_PORT', '5432'),
     }
 }
     
