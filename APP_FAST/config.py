5746a7084aa7ee14e1b2fc0fa3e911be875e1c6b
Dong-Han <donghk77@naver.com>
2025-02-10 19:59:48 +0900

    fix

--- APP_FAST/config.py
+++ APP_FAST/config.py
@@ -1,7 +1,7 @@
 from pydantic_settings import BaseSettings  # ✅ 변경된 import 경로
 
 class Settings(BaseSettings):
-    DATABASE_URL: str = "postgresql+asyncpg://postgres:0000@localhost:5432/app_fast"
+    DATABASE_URL: str = "postgresql+asyncpg://postgres:0000@docker-fast_db-1:5432/app_fast"
     SECRET_KEY: str = 'django-insecure-+&2r64c8n7j&xc=q1x=@dt3kiq!8=_y&5ux$li+(0ndw_r$=5$'
     ACCESS_TOKEN_EXPIRE_MINUTES: int = 3000
 
