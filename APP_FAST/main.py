609508826625f3453f7c9207f20d223e18fe8d52
Dong-Han <donghk77@naver.com>
2025-02-10 22:37:53 +0900

    fix

--- APP_FAST/main.py
+++ APP_FAST/main.py
@@ -9,7 +9,7 @@ app.include_router(posts.router, prefix='/posts')
 
 # Prometheus 모니터링
 instrumentator = Instrumentator()
-instrumentator.instrument(app).expose(app, endpoint="/metrics")
+instrumentator.instrument(app).expose(app, endpoint="/api/metrics")
 
 # 애플리케이션 시작 시 테이블 생성
 @app.on_event("startup")
